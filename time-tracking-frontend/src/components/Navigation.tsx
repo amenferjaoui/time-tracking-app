b72247907b0cb868b844e7076af877c76ccf307d
chekir.cherifa <cherifachekir9@gmail.com>
2025-02-10 17:06:59 +0100

    fix 2.0 package import

--- time-tracking-frontend/src/components/Navigation.tsx
+++ time-tracking-frontend/src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 import { User } from "../types";
 import { authApi } from "../services/api";
+import { FiLogOut, FiUser } from "react-icons/fi"; // Icône pour l'Utilisateur
 
 import logo from '../assets/logo.png';
 
@@ -28,7 +29,7 @@ export default function Navigation({ user, onLogout }: Props) {
             </div>
             <div className="nav-user-actions">
               <div className="user-info">
-              
+                <FiUser size={24} />
                 <div className="user-details">
                   <span className="username">{user.username}</span>
                   <span className="role">({user.role})</span>
@@ -58,7 +59,7 @@ export default function Navigation({ user, onLogout }: Props) {
             </div>
           </div>
           <button onClick={handleLogout} className="logout-button">
-            
+            <FiLogOut size={20} />
             Déconnexion
           </button>
         </div>
